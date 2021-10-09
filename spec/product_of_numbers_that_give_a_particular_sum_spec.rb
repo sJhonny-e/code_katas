5bb986339b950c38a7034dbb50e254bcb32beada
sJhonny-e <notReal@jhonnye.com>
2021-10-09 10:57:48 +0300

    fix typo

--- spec/product_of_numbers_that_give_a_particular_sum_spec.rb
+++ spec/product_of_numbers_that_give_a_particular_sum_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../app/product_of_numbers_that_give_a_particular_sum'
 
 describe 'Generating a product of 2 numbers from a list. The numbers are chosen for having a specific sum' do
-    it 'provides the product of 1721 and 299, since they sum up to 2000' do
+    it 'provides the product of 1721 and 299, since they sum up to 2020' do
         expect(give_product([1721,979, 366, 299, 675, 1456])).to eq(1721 * 299)
     end
 end
