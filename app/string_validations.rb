2706885ca2381df0fa3971a88fa2e522fa848335
sJhonny-e <notReal@jhonnye.com>
2021-10-09 12:34:31 +0300

    added basic validation for lower / upper limits

--- app/string_validations.rb
+++ app/string_validations.rb
@@ -1,6 +1,7 @@
 def valid_string?(str)
     lower_bound = str[0].to_i
     upper_bound = str[2].to_i
+    return false unless lower_bound.to_s == str[0] && upper_bound.to_s == str[2] 
 
     split_str = str.split(':')
     letter_to_find = split_str[0][-1]
--- spec/string_validations_spec.rb
+++ spec/string_validations_spec.rb
@@ -17,4 +17,14 @@ describe 'validating that the given character exists within the given range in a
     it 'validates that "c" exist between 2 and 9 times in "2-9 c: ccccccccc"' do
         expect(valid_string?('2-9 c: ccccccccc')).to eq(true)
     end
+
+    context 'invalid input' do
+        it 'is false if lower bound is larger than upper bound' do
+            expect(valid_string?('3-1 b: cdebb')).to eq(false)
+        end
+
+        it 'is false if one of the bounds isnt numeric' do
+            expect(valid_string?('a-3 b: bcdefgbb')).to eq(false)
+        end
+    end
 end
