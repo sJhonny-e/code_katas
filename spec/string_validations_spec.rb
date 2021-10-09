1ce9e7ea0e13360dbbd3c2b47350255583a661ec
sJhonny-e <notReal@jhonnye.com>
2021-10-09 12:27:25 +0300

    added more examples

--- spec/string_validations_spec.rb
+++ spec/string_validations_spec.rb
@@ -5,4 +5,12 @@ describe 'validating that the given character exists within the given range in a
     it 'validates that "a" exists between 1 and 3 times in "abcde"' do
         expect(valid_string?('1-3 a: abcde')).to eq(true)
     end
+
+    it 'validates that "b" does not exist between 1 and 3 times in "cdefg"' do
+        expect(valid_string?('1-3 b: cdefg')).to eq(false)
+    end
+
+    it 'validates that "c" exist between 2 and 9 times in "2-9 c: ccccccccc"' do
+        expect(valid_string?('2-9 c: ccccccccc')).to eq(true)
+    end
 end
