ea91167c28b8f8e9d7567fadc20d396c96d360a7
sJhonny-e <notReal@jhonnye.com>
2021-10-09 11:25:09 +0300

    refactor: use new generic method

--- app/product_of_numbers_that_give_a_particular_sum.rb
+++ app/product_of_numbers_that_give_a_particular_sum.rb
@@ -1,10 +1,7 @@
 def give_product(numbers)
     desired_sum = 2020
-    numbers.find.with_index do |n, i|
-        other_numbres = numbers.dup.tap{|new_arr| new_arr.delete_at(i)}
-        complementary_number = other_numbres.find {|other_n| n + other_n == desired_sum}
-        return complementary_number * n if complementary_number
-    end
+    elements = do_find_numbers(numbers, desired_sum, 2)
+    elements.reduce(1) {|prod, n| prod * n}
 end
 
 def give_product_three(numbers)
